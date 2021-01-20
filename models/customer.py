990873b58ce1bdb6b0c53442bd8a208ed5956377
Jake Butler <tjakebutler@gmail.com>
2021-01-20 14:03:26 -0600

    updated the animals/request.py module with the def update_animal also updated the def do_PUT inside of the request_handler.py

--- models/customer.py
+++ models/customer.py
@@ -3,6 +3,8 @@
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
+
+    # This is a blueprint - this is the code we use to instanciate a new customer, this is turning SQL code into a Customer (type)
    
 class Customer():
 
